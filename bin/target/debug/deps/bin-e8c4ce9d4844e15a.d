f16e40d79e02be7ad68114347c50b0b8e002fec7
andreishark <andreishark10@gmail.com>
2023-05-31 22:12:57 +0300

    Adding exceptions to target folder

--- bin/target/debug/deps/bin-e8c4ce9d4844e15a.d
+++
@@ -1,8 +0,0 @@
-/run/media/andreishark/Stuff/VS Code/rust/templateR/bin/target/debug/deps/bin-e8c4ce9d4844e15a.rmeta: src/main.rs Cargo.toml
-
-/run/media/andreishark/Stuff/VS Code/rust/templateR/bin/target/debug/deps/bin-e8c4ce9d4844e15a.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
